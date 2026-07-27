742a39f7f8650bc5e5a0c8eba0d0133c5a9ad883
brigit <brigit@DESKTOP-5DKQJUL.localdomain>
2026-07-27 12:20:18 +0300

    Complete HTML Forms TaskLister Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const task = input.value.trim();
+
+    if (task !== "") {
+      const li = document.createElement("li");
+      li.textContent = task;
+      taskList.appendChild(li);
+
+      input.value = "";
+    }
+  });
 });
